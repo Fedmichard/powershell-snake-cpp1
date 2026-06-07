1312a71ec768350d64a99b2db015104e034b20fd
Fedmichard <francoisfedmichard@gmail.com>
2026-06-07 19:39:34 -0400

    Was able to get another tail to generate but not correctly passing previous location so that it appears at the end.

--- Player.cpp
+++ Player.cpp
@@ -88,7 +88,7 @@ void Player::DrawPlayer(std::vector<std::vector<int>>& map, int x, int y) {
     // if the player hits a food item
   } else if (map[pos.y].at(pos.x) == 3) {
     if (!tailOne->next) {
-      Tail* newTail = new Tail({ .x = 24, .y = 12 });
+      Tail* newTail = new Tail({ .x = previousX, .y = previousY });
 
       tailOne->next = newTail;
     }
@@ -103,5 +103,59 @@ void Player::DrawPlayer(std::vector<std::vector<int>>& map, int x, int y) {
     };
 
     tailOne->Draw(map, previousX, previousY);
+  }
+}
+
+void Tail::Draw(std::vector<std::vector<int>>& map, int x, int y) {
+  // Current x and y pos
+  int newPosX = x;
+  int newPosY = y;
+
+  // Update player pos
+  int rows = map.size();
+  int cols = map[0].size();
+
+  if (x >= cols) {
+        newPosX = x % cols;
+  } else if (y >= rows) {
+        newPosY = y % rows;
+  }
+
+  int previousX = position.x;
+  int previousY = position.y;
+
+  if (previousY >= rows - 3) {
+        previousY = previousY % rows;
+  } else if (previousX >= cols - 3) {
+        previousX = previousX % cols;
+  }
+
+  // Can probably use % to create snake like mechanic
+  // I believe we -2 because the player doesn't start at the first level or last on either X or Y
+  // at pos 0 there will be a # to represent the wall
+  // at the last pos there will be a # to represent the wall
+  if (newPosY >= rows - 2) {
+        newPosY = newPosY % rows;
+  } else if (previousX >= cols - 2) {
+        newPosX = newPosX % cols;
+  }
+
+  // If the preivous position x, y wasn't a wall
+  if (map[previousY].at(previousX) != 1) {
+        // Set it's previous position as 0 so the next time it's drawn, there'll be a blank spot
+        map[previousY].at(previousX) = 0;
+  }
+
+  position.x = newPosX;
+  position.y = newPosY;
+
+  // As long as there is a 0 (for empty space), draw player
+  if (map[position.y].at(position.x) == 0) {
+        // Y = row; X = column
+        map[position.y].at(position.x) = 2;
+  }
+
+  if (next != nullptr) {
+        next->Draw(map, previousX, previousY - 1);
   }
 }
--- Player.h
+++ Player.h
@@ -37,57 +37,5 @@ struct Tail {
                 position = pos;
         }
 
-        void Draw(std::vector<std::vector<int>>& map, int x, int y) {
-                // Current x and y pos
-                int newPosX = x;
-                int newPosY = y;
-
-                // Update player pos
-                int rows = map.size();
-                int cols = map[0].size();
-
-                if (x >= cols) {
-                        newPosX = x % cols;
-                } else if (y >= rows) {
-                        newPosY = y % rows;
-                }
-
-                int previousX = position.x;
-                int previousY = position.y;
-
-                if (previousY >= rows - 2) {
-                        previousY = previousY % rows;
-                } else if (previousX >= cols - 2) {
-                        previousX = previousX % cols;
-                }
-
-                // Can probably use % to create snake like mechanic
-                // I believe we -2 because the player doesn't start at the first level or last on either X or Y
-                // at pos 0 there will be a # to represent the wall
-                // at the last pos there will be a # to represent the wall
-                if (newPosY >= rows - 2) {
-                        newPosY = newPosY % rows;
-                } else if (previousX >= cols - 2) {
-                        newPosX = newPosX % cols;
-                }
-
-                // If the preivous position x, y wasn't a wall
-                if (map[previousY].at(previousX) != 1) {
-                        // Set it's previous position as 0 so the next time it's drawn, there'll be a blank spot
-                        map[previousY].at(previousX) = 0;
-                }
-
-                position.x = newPosX;
-                position.y = newPosY;
-
-                // As long as there is a 0 (for empty space), draw player
-                if (map[position.y].at(position.x) == 0) {
-                        // Y = row; X = column
-                        map[position.y].at(position.x) = 2;
-                }
-
-                if (next != nullptr) {
-                        next->Draw(map, previousX, previousY);
-                }
-        }
+        void Draw(std::vector<std::vector<int>>& map, int x, int y);
 };
